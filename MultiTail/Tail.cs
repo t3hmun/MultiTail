c490836de1aed6179383b9cd0b2058f224316350
t3hmun <git@t3hmun.com>
2018-03-31 22:21:24 +0100

    Well i just realised I'm going about this the wrong way.
    I neeed to write partial lines to the console.
    There is no way of telling if the last line read from a log is partial
    or complete, not all logs insert newlines after every message
    But with mutliple logs i cant just overwrite the last output line.
    So the window writer just got a heck of a lot more complicated.
    WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH.

--- MultiTail/Tail.cs
+++ MultiTail/Tail.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Threading;
 
     internal class Tail : IDisposable
@@ -23,20 +24,15 @@
             _fs = new FileStream(settings.Filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
             _sr = new StreamReader(_fs);
 
+            var tail = ReadTailLinesWithLastLineIncomplete(initialReadAmountFromEnd);
 
-            // The queue allows us to run through the file without loading all of it to memory.
-            // Not really necessary most of the time.
-            // While I could seek the underlying filw stream, finding lines gets complicated and not worth the effort.
-            // Personally I don't care about this overhead.
-            var queue = new Queue<string>();
-            string line;
-            while ((line = _sr.ReadLine()) != null)
+            for (var i = 0; i < tail.Length - 1; i++)
             {
-                queue.Enqueue(line);
-                if (queue.Count > initialReadAmountFromEnd) queue.Dequeue();
+                var lineToWrite = tail[i];
+                _writeLine(lineToWrite);
             }
 
-            foreach (var lineToWrite in queue) _writeLine(lineToWrite);
+            string partialLine = tail.LastOrDefault();
 
             _fsw = new FileSystemWatcher
             {
@@ -44,15 +40,11 @@
                 Filter = settings.Filename
             };
 
-            _fsw.Changed += (obj, args) =>
-            {
-                _waiter.Set();
-            };
+            _fsw.Changed += (obj, args) => { _waiter.Set(); };
             _fsw.EnableRaisingEvents = true;
-            
-            _thread = new Thread(ReadUntilKill);
+
+            _thread = new Thread(() => ReadUntilKill(partialLine));
             _thread.Start();
-            
         }
 
         public void Dispose()
@@ -65,20 +57,91 @@
             _fs?.Dispose();
         }
 
+        private string[] ReadTailLinesWithLastLineIncomplete(int linesFromEndToKeep)
+        {
+            // Seeking position in a file that could be any text encoding isn't trivial.
+            // StreamReader.ReadLine is wrong, it wont tell you if the last line ended (has a \n).
+            // So I've chosen to read the whole file in alternating buffers, its good enough.
+            // Also we have no idea where in the file the last x lines start.
+            
+            // 1048576 is a mebibyte so 16 mebibytes of ram (16 bit char).
+            // So 2 buffers will allocate 32mib of RAM before starting to read what will probably be a 1kib log file.
+            // Seems legit.
+            // This baroque contraption should quickly ignore most of a 1gb monster log.
+            // But no so efficiently that I have to think about how encoding works (and then do it wrong anyway).
+            const int bufferLen = 1048576;
+            
+            int charsRead;
+            var bufferA = new char[bufferLen];
+            var bufferB = new char[bufferLen];
+            char[] penultimatePiece;
+            var lastPiece = bufferB;
+            long totalLen = 0; // an int will only store 1gib of chars.
+            do
+            {
+                charsRead = _sr.ReadBlock(bufferA, 0, bufferLen);
+                totalLen += charsRead;
+                penultimatePiece = lastPiece;
+                lastPiece = bufferA;
+
+                if (charsRead != bufferLen) continue;
+                
+                charsRead = _sr.ReadBlock(bufferB, 0, bufferLen);
+                totalLen += charsRead;
+                penultimatePiece = lastPiece;
+                lastPiece = bufferB;
+
+            } while (charsRead == bufferLen);
+
+            var vaguelySensibleNumOfChars = 16384; //32 kibibytes, this many chars fills my monitor, close enough.
+            if (totalLen < vaguelySensibleNumOfChars) vaguelySensibleNumOfChars = (int)totalLen;
+
+            var text = charsRead > bufferLen
+                ? new string(penultimatePiece.Concat(lastPiece.Take(charsRead))
+                    .Take(vaguelySensibleNumOfChars)
+                    .ToArray())
+                : new string(lastPiece.Take(vaguelySensibleNumOfChars).ToArray());
+
+            var lines = text.Split('\n');
+            var lineCount = lines.Length;
+            var start = lineCount < linesFromEndToKeep ? 0 : lineCount - linesFromEndToKeep;
+            return lines.Skip(start).ToArray();
+        }
+
         public void Update()
         {
             _waiter.Set();
         }
 
-        private void ReadUntilKill()
+        private void ReadUntilKill(string partialLine)
         {
+            // We don't use readline because it reads incomplete lines without telling us that there was no \n.
             while (!_kill)
             {
-                var line = _sr.ReadLine();
-                if (line == null)
+                var text = _sr.ReadToEnd();
+                if (text == "")
+                {
                     _waiter.WaitOne();
-                else
-                    _writeLine(line);
+                }
+                else if (!text.Contains('\n'))
+                {
+                    //If it doesn't contain a newline then it isn't a new line.
+                    partialLine += text; //null+="..." seems to work fine, who knew?
+                }
+
+                text = text.Replace("\r", "");
+                    var lines = text.Split('\n');
+
+                    var line = _sr.ReadLine();
+                    if (line == null)
+                    {
+                        _waiter.WaitOne();
+                    }
+                    else
+                    {
+                        _writeLine(line);
+                    }
+                }
             }
         }
     }
